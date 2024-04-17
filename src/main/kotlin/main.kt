f3cee51f1b1e3020f232083728bb818cfbc0c7f8
Joao GS Pereira <joaogspe@outlook.com>
2024-04-17 08:13:54 -0300

    :sparkles: feat: create a http request function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+
 fun main() {
-    println("Hello, World!")
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+    println(response.body())
 }
